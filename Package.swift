283ef01e08a8108a24bd4f326212f911a8e5f8fe
Sina Khanjani <Sinakhanjani@gmail.com>
2020-04-01 00:27:44 +0430

    changed here

--- Package.swift
+++ Package.swift
@@ -18,7 +18,8 @@ let package = Package(
         .package(url: "https://github.com/vapor/redis.git", from: "3.0.0"),
 //        .package(url: "https://github.com/tid-kijyun/Kanna.git", from: "5.2.0"),
         .package(url: "https://github.com/scinfu/SwiftSoup.git", from: "1.7.4"),
-        .package(url: "https://github.com/mongodb/mongo-swift-driver.git", from: "0.1.3")
+//        .package(url: "https://github.com/mongodb/mongo-swift-driver.git", from: "0.1.3"),
+        .package(url: "https://github.com/asensei/vapor-fluent-mongo.git", from: "3.0.0"),
     ],
     targets: [
         .target(name: "App", dependencies: ["FluentPostgreSQL",
@@ -27,7 +28,7 @@ let package = Package(
                                             "Authentication",
                                             "JWT",
                                             "Redis",
-                                            "SwiftSoup","MongoSwift"]),
+                                            "SwiftSoup","FluentMongo"]),//"MongoSwift"
         .target(name: "Run", dependencies: ["App"]),
         .testTarget(name: "AppTests", dependencies: ["App"]),
     ]
---
+++ Sources/App/Models/Currency/CurrencyBuilder.swift
@@ -0,0 +1,16 @@
+//
+//  CurrencyBuilder.swift
+//  App
+//
+//  Created by Sina khanjani on 1/12/1399 AP.
+//
+
+import Foundation
+
+class CurrencyBuilder {
+    var currency: Currency
+    
+    init(currency: Currency) {
+        self.currency = currency
+    }
+}
